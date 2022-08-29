2066a2814ce54aebd7412ec47a18a8998d129727
anastasion88 <anastasionma@gmail.com>
2022-08-28 23:02:23 -0500

    updates in the app

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+import styled from 'styled-components/native';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from './src/infrastructure/theme';
+
+// const isAndroid = Platform.OS === 'android';
 
 export default function App() {
+  const [oswaldLoadded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoadded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoadded || !latoLoadded) {
+    return null;
+  }
+
+  // <></>- fragments allow to render 2 elements side by side without wrapping in another container
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { RestaurantInfoCard } from '../components/restaurant-info-card';
+import styled from 'styled-components/native';
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar placeholder="Search" />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
